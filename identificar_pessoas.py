4d6664b7b9cc0425d2dc80369dfc33a710ff734f
Alice <alicebianchi2013@gmail.com>
2024-04-20 22:57:48 -0300

    Convertendo o frame de BGR para RGB, armazenando os dados da pose, como pontos x, y e z dos landmarks e criando as conecção entre os pontos do landmark

--- identificar_pessoas.py
+++ identificar_pessoas.py
@@ -15,3 +15,13 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             print("Ignoring No Video in Camera frame")
             continue
         
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        results = pose.process(image)
+        
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245,117,66), thickness=4, circle_radius=2),
+                                  mp_drawing.DrawingSpec(color=(245,66,230), thickness=4)
+                                  )
+        
+        
