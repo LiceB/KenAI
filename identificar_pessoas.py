6ae989bca39d6b50fdf32cd0c86fb7293b05bfc5
Alice <alicebianchi2013@gmail.com>
2024-04-20 22:47:35 -0300

    definindo os valores para a confiança da detecção e para o movimento, criando um loop enquanto a camera estiver aberta e em caso de erro o código é finalizado

--- identificar_pessoas.py
+++ identificar_pessoas.py
@@ -6,3 +6,12 @@ cap = cv2.VideoCapture(0)
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        if not ret:
+            print("Ignoring No Video in Camera frame")
+            continue
+        
