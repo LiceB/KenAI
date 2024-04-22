3468e67aefd7756fb41029632a33a86a472a314e
Alice <alicebianchi2013@gmail.com>
2024-04-22 01:29:40 -0300

    código base para reconhecer marcações de pessoas sozinhas

--- identificar_pessoas.py
+++ identificar_pessoas.py
@@ -1,34 +1,27 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 
-cap = cv2.VideoCapture(0)
-
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
+mp_draw = mp.solutions.drawing_utils
+pose = mp_pose.Pose()
+cap = cv2.VideoCapture("videos/video4.mp4")
+# cap = cv2.VideoCapture(0)
 
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        
-        if not ret:
-            print("Ignoring No Video in Camera frame")
-            continue
-        
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        
-        results = pose.process(image)
-        
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color=(245,117,66), thickness=4, circle_radius=2),
-                                  mp_drawing.DrawingSpec(color=(245,66,230), thickness=4)
-                                  )
-        
-        print(results.pose_landmarks)
-        cv2.imshow("Mediapipe feed", image)
-        
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-
+while True:
+    ret, img = cap.read()
+    img = cv2.resize(img, (600, 400))
+    
+    results = pose.process(img)
+    mp_draw.draw_landmarks(img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                           mp_draw.DrawingSpec((255, 0, 0), 2, 2),
+                           mp_draw.DrawingSpec((255, 0, 255), 2, 2))
+    
+    cv2.imshow("Pose estimation", img)
+    
+    print(results.pose_landmarks) 
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
 cap.release()
 cv2.destroyAllWindows()
