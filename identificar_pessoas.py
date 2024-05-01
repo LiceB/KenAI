95e24e52116ba5c3a676126369481136c24c1689
Alice Bianchi Nogueira <alicebianchi2013@gmail.com>
2024-05-01 02:03:01 -0300

    Código base para reconhecimento de corpo

--- identificar_pessoas.py
+++ identificar_pessoas.py
@@ -1,27 +1,58 @@
-import cv2
 import mediapipe as mp
-import numpy as np
+import cv2
 
-mp_pose = mp.solutions.pose
-mp_draw = mp.solutions.drawing_utils
-pose = mp_pose.Pose()
-cap = cv2.VideoCapture("videos/video4.mp4")
-# cap = cv2.VideoCapture(0)
+def inicia_mediapipe():
+    # Inicialização do MediaPipe Pose
+    mp_pose = mp.solutions.pose
+    pose = mp_pose.Pose()
+    return pose, mp_pose
 
-while True:
-    ret, img = cap.read()
-    img = cv2.resize(img, (600, 400))
+# Função para processar cada frame do vídeo
+def process_frame(image, pose, mp_pose):
+    # Converta a imagem para RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
     
-    results = pose.process(img)
-    mp_draw.draw_landmarks(img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                           mp_draw.DrawingSpec((255, 0, 0), 2, 2),
-                           mp_draw.DrawingSpec((255, 0, 255), 2, 2))
+    # Processamento de detecção de pose
+    results = pose.process(image)
     
-    cv2.imshow("Pose estimation", img)
+    # Converta a imagem de volta para BGR para exibição
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     
-    print(results.pose_landmarks) 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-cap.release()
-cv2.destroyAllWindows()
+    if results.pose_landmarks:
+        landmarks = results.pose_landmarks.landmark
+        
+        for landmark in landmarks:
+            x = int(landmark.x * image.shape[1])
+            y = int(landmark.y * image.shape[0])
+            cv2.circle(image, (x, y), 5, (0, 255, 0), -1)
+
+    return image
+
+def main():
+    # cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture("videos/video.mp4")
+    pose, mp_pose = inicia_mediapipe()
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            break
+        
+        # Redimensiona o frame
+        frame = cv2.resize(frame, (640, 480))
+
+        # Processar o frame
+        frame = process_frame(frame, pose, mp_pose)
+        
+        # Exibir o frame
+        cv2.imshow('Pose', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
