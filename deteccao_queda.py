ba82872c8820a2cfcc3284967a85825e2a4fcddc
Alice Bianchi Nogueira <alicebianchi2013@gmail.com>
2024-05-22 18:27:02 -0300

    adicionando funcionalidade de text to speech para avisar sobre a queda

--- deteccao_queda.py
+++ deteccao_queda.py
@@ -1,9 +1,11 @@
 import cv2
 import time
 import mediapipe as mp
+import pyttsx3
+import threading
 
 # Inicializa a captura do vídeo
-video_path = "videos/Vídeo.mp4"
+video_path = "videos/video.mp4"
 cap = cv2.VideoCapture(video_path)
 
 # Verifica se o vídeo está aberto corretamente
@@ -11,11 +13,25 @@ if not cap.isOpened():
     print("Erro ao abrir o vídeo")
     exit()
 
-# Define a taxa de atualização da flag em segundos
-update_interval = 2
+# Define a duração que a mensagem "Queda!" deve ser exibida (em segundos)
+message_duration = 3
 
-# Inicializa o tempo de referência
-start_time = time.time()
+# Inicializa o tempo da última detecção de queda e o tempo da última repetição do áudio
+last_fall_time = None
+last_audio_time = None
+
+# Inicializa o mecanismo de text-to-speech
+engine = pyttsx3.init()
+
+# Função para falar "Queda!"
+def falar_queda():
+    engine.say("Queda!")
+    engine.runAndWait()
+
+# Função para iniciar a fala em um thread separado
+def falar_queda_thread():
+    thread = threading.Thread(target=falar_queda)
+    thread.start()
 
 # Inicializa o detector de pose do MediaPipe
 mp_pose = mp.solutions.pose
@@ -24,6 +40,9 @@ pose = mp_pose.Pose()
 # Inicializa variáveis para armazenar a posição Y do nariz nos frames anteriores
 prev_nose_y = None
 prev_prev_nose_y = None
+
+# Variável para controlar a primeira detecção
+first_fall_detected = False
 
 while True:
     # Captura frame-by-frame
@@ -60,8 +79,19 @@ while True:
         
         # Verifica se o nariz está próximo ao nível do pé
         if abs(nose_y - left_foot_y) < proximity_limit or abs(nose_y - right_foot_y) < proximity_limit:
-            #print("Queda detectada!")
-            cv2.putText(frame, "Queda!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            current_time = time.time()
+            if not first_fall_detected:
+                last_fall_time = current_time
+                last_audio_time = current_time
+                first_fall_detected = True
+                falar_queda_thread()  # Fala "Queda!" na primeira detecção em um thread separado
+            elif (current_time - last_audio_time) >= 5:
+                last_audio_time = current_time
+                falar_queda_thread()  # Fala "Queda!" novamente após 5 segundos em um thread separado
+
+    # Verifica se a mensagem de queda deve ser exibida
+    if first_fall_detected and (time.time() - last_fall_time) < message_duration:
+        cv2.putText(frame, "Queda!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
     # Mostra o frame
     cv2.imshow('Detecção de queda', frame)
